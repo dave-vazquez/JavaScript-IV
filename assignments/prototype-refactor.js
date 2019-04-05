ab8883942671b52675ae49b3edb388566c487097
dmvazli <davidmvazquez1@gmail.com>
2019-04-04 23:24:08 -0400

    refactored to ES6 classes

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,204 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+
+class GameObject {
+    constructor(attr){
+        this.createdAt = attr.createdAt;
+        this.name = attr.name;
+        this.dimensions = attr.dimensions;
+    }
+
+    destroy() {
+        return `${this.name} was removed from the game.`;
+    }
+}
+  
+class CharacterStats extends GameObject {
+    constructor(attr) {
+        super(attr);
+        this.healthPoints = attr.healthPoints;
+    }
+
+    takeDamage() {
+        return `${this.name} took damage`;
+    }
+}
+  
+class Humanoid extends CharacterStats {
+    constructor(attr) {
+        super(attr);
+        this.team = attr.team;
+        this.weapons = attr.weapons;
+        this.language = attr.language;
+    }
+
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}`;
+    }
+}
+  
+
+const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+        'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+});
+  
+const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+        'Giant Sword',
+        'Shield',
+    ],
+    language: 'Common Tongue',
+});
+  
+const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+        'Bow',
+        'Dagger',
+    ],
+    language: 'Elvish',
+});
+    
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  
+  
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+class Villain extends Humanoid{
+    constructor(attr) {
+        super(attr);
+    }
+
+    clobber(foe) {
+        let weaponNum = Math.round(Math.random());
+        
+        weaponNum === 0 ? foe.healthPoints -= 3 : foe.healthPoints -= 2;
+        
+        console.log(`${this.name} used ${this.weapons[weaponNum]} on ${foe.name}`);
+        
+        foe.takeDamage();
+        
+        if (foe.healthPoints <= 0) {
+            console.log(foe.destroy());
+        }
+    }
+}
+  
+class Hero extends Humanoid{
+    constructor(attr) {
+        super(attr);
+    }
+
+    slay(foe) {
+        
+        let weaponNum = Math.round(Math.random());
+        
+        weaponNum === 0 ? foe.healthPoints -= 5 : foe.healthPoints -= 4;
+        
+        console.log(`${this.name} used ${this.weapons[weaponNum]} on ${foe.name}! `);
+        
+        foe.takeDamage();
+        
+        if (foe.healthPoints <= 0) {
+            console.log(foe.destroy());
+        }
+    }
+}
+  
+const ogre = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+        length: 4,
+        width: 5,
+        height: 10,
+    },
+    healthPoints: 50,
+    name: 'Grok',
+    team: 'Cave of Doom',
+    weapons: [
+        'Club',
+        'Fist',
+    ],
+    language: 'Ogric',
+});
+  
+const dave = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 3,
+        height: 8,
+    },
+    healthPoints: 30,
+    name: 'Dave',
+    team: '',
+    weapons: [
+        'Sword',
+        'Darts',
+    ],
+    language: 'Math',
+});
+  
+  
+let battle = setInterval(() => {
+
+    if(Math.round(Math.random()* 2) === 1) {
+        ogre.clobber(dave);
+        console.log(`${dave.name} has ${dave.healthPoints} HP left!`);
+        console.log('\n');
+    }
+    else {
+        dave.slay(ogre);
+        console.log(`${ogre.name} has ${ogre.healthPoints} HP left!`);
+        console.log('\n');
+    }
+
+    if(dave.healthPoints <= 0 || ogre.healthPoints <= 0) {
+        clearInterval(battle);
+    }
+
+}, 2000);
+  
